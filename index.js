b4c9712d0a14908cfed2a787c39c638bc0d3c9cb
springercass <casprin2@gmail.com>
2019-07-09 00:19:16 -0500

    get

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ server.post("/api/users", (req, res) => {
   if (!name || !bio) {
     res
       .status(400)
-      .json({ errorMessage: "Please provide name and bio for the user." });
+      .json({ error: "Please provide name and bio for the user." });
   } else {
     Users.insert(req.body)
       .then(user => {
@@ -24,9 +24,38 @@ server.post("/api/users", (req, res) => {
       })
       .catch(() => {
         res.status(500).json({
-          errorMessage:
-            "There was an error while saving the user to the database."
+          error: "There was an error while saving the user to the database."
         });
       });
   }
+});
+
+server.get("/api/users", (req, res) => {
+  Users.find()
+    .then(Users => {
+      res.status(200).json(users);
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: "The user's information could not be retrieved."
+      });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  Users.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved." });
+    });
 });
