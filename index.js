9135fd2caf3ff540c25df3fab3594476aa4a7fc8
springercass <casprin2@gmail.com>
2019-07-09 00:08:43 -0500

    post

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 // implement your API here
 const express = require("express");
+const Users = require("./data/db.js");
 
 const server = express();
+server.use(express.json());
 
-server.listen(4000, () => {
-  console.log("\n** api up and running on port 4k **");
+const port = 5000;
+server.listen(port, () => {
+  console.log(`\n** API up and running on ${port} **`);
+});
+
+server.post("/api/users", (req, res) => {
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    Users.insert(req.body)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage:
+            "There was an error while saving the user to the database."
+        });
+      });
+  }
 });
