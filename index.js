f38d98bbe2578cc675df07f384c2cacae5521ed0
springercass <casprin2@gmail.com>
2019-07-09 10:08:17 -0500

    put, mvp complete

--- index.js
+++ index.js
@@ -75,3 +75,29 @@ server.delete("/api/users/:id", (req, res) => {
       res.status(500).json({ error: "The user could not be removed." });
     });
 });
+
+server.put("/api/users/:id", (req, res) => {
+  const { name, bio } = req.info;
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ error: "Please provide name and bio for the user." });
+  } else {
+    Users.update(req.params.id, req.info)
+      .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ erroer: "The user information could not be modified." });
+      });
+  }
+});
